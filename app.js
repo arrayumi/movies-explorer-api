1a53a502f7947ce27e7f9b788d62ed93ca441dae
Anna <thoria@yandex.ru>
2023-08-17 11:12:42 +0300

    feat: добавить модели movie и user

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
