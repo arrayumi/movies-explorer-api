595c0632f563fd6aac34104b73d426cf3b76cf78
Anna <thoria@yandex.ru>
2023-09-08 15:02:14 +0300

    hotfix: обновить regexLink

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 
-const regexLink = /(https?:\/\/.*\.(?:png|jpg|jpeg))/;
+const regexLink = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 // login and register
 const validateCreateUser = celebrate({
